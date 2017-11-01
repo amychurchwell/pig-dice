bd84e764b09e6d99650a791c5ba30196339edd42
Amy Churchwell and Lew Davidson <amy@>
2017-11-01 13:00:37 -0700

    debug scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,45 @@
 //back-end//
 
-function Player(name, round, total){
+function Player(name){
   this.name = name;
-  this.round = round;
-  this.total = total;
-  this.roll = diceRoll;
+  this.round = [];
+  this.total = 0;
+  this.roll = 0;
 };
 
 function diceRoll(){
   var answer = Math.floor((Math.random() * 6) + 1)
-  answer + this.round;
   if (answer === 1){
     this.round === 0;
-    return "test";
+    return "PLAYER TURN IS OVER.";
   } else {
     return answer;
-  };
 };
 
+// function addScores(){
+//
+// }
 
+var newPlayer = new Player("holkuj");
 
 //front-end//
-$(function(){
+$(document).ready(function(){
   $("#form-p1").submit(function(event){
     event.preventDefault();
-    var player1 = new Player(name,1,0);
-    var player2 = new Player(name,1,0);
+    // var finishedScore =
+
     // player1.name = $("input#p1name").val();
     // $("#p1-title").text(player1.name);
-    $('#result').text(diceRoll());
+    $('#result').text(newPlayer.roll());
 
-player1.roll();
-
-
-
+    // $("#form-p2").submit(function(event){
+    //   event.preventDefault();
+    //   var player2 = new Player(name, 0);
+    // });
+    //
+    //   // player1.name = $("input#p1name").val();
+    //   // $("#p1-title").text(player1.name);
+    //   $('#result').text(player2.roll());
+    //   $('#p2score').text();
   });
 });
