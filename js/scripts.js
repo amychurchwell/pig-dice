39f79607c95b73625f5e2556632ed4f389ac5b06
Amy Churchwell and Lew Davidson <lew@>
2017-11-01 10:39:12 -0700

    debug dice roll function add player2 form .etc

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,38 @@
 //back-end//
 
+function Player(name, round, total){
+  this.name = name;
+  this.round = round;
+  this.total = total;
+  this.roll = diceRoll;
+};
+
+function diceRoll(){
+  var answer = Math.floor((Math.random() * 6) + 1)
+  answer + this.round;
+  if (answer === 1){
+    this.round === 0;
+    return "test";
+  } else {
+    return answer;
+  };
+};
+
+
 
 //front-end//
-$(function()){
-  $('#form').submit(function(event)){
+$(function(){
+  $("#form-p1").submit(function(event){
     event.preventDefault();
+    var player1 = new Player(name,1,0);
+    var player2 = new Player(name,1,0);
+    // player1.name = $("input#p1name").val();
+    // $("#p1-title").text(player1.name);
+    $('#result').text(diceRoll());
 
-    var action = $(input:radio[name="select"]:checked).val();
+player1.roll();
+
+
 
   });
 });
