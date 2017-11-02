a9cb7037ee4db97c4e9355565e57dd15828931fc
Amy Churchwell and Lew Davidson <amy@>
2017-11-02 10:21:09 -0700

    roll and score object storage successfully debugged

--- js/scripts.js
+++ js/scripts.js
@@ -1,53 +1,50 @@
 //back-end//
-$(document).ready(function(){
 
-  function Player(name, total){
+  function Player(name, total, round){
     this.name = name;
     this.total = total;
-    round = [];
+    this.round = 0;
   };
 
-  function score(){
-    var total = 0;
-    for(var i = 0, len = round.length; i < len; i++) {
-    total += round[i];
-   };
-    return total;
-  };
+  // Player.prototype.score = function(){
+  //   var total = 0;
+  //   for(var i = 0, len = round.length; i < len; i++) {
+  //   total += this.round[i];
+  //  };
+  //   return total;
+  // };
 
-  function diceRoll(){
+  Player.prototype.diceRoll = function(){
     var result = Math.floor((Math.random() * 6) + 1);
-    if (result === 1) {
-      return "BYE YOU SUCK!"
-      round = [];
-    } else {
-    round.push(result);
     return result;
-    }
   };
 
-  function endScore(){
-    var result = score(+ this.total);
-    round = [];
-    if (result >= 100) {
-      return "YAY YOU WIN GET LOST!"
-    } else {
-     return result;
-   }
- };
+
+  Player.prototype.endScore = function(){
+    score(this.round) + this.total;
+  }
+
 
 //front-end//
-  var newPlayer = new Player();
+var newPlayer = new Player();
 
+  $(document).ready(function(){
   $('#p1-roll').click(function(event){
     event.preventDefault();
     // var nameInput = $('#p1name').val();
 
+    var result = newPlayer.diceRoll()
 
-    $('#result').text(diceRoll());
-    console.log(round);
-    console.log(score());
 
+    if (result === 1){
+      newPlayer.round = 0;
+      $('#result').text("LOSE");
+    }else{
+      newPlayer.round += result;
+      $('#result').text(result);
+    }
+
+    $('#p1total').text(newPlayer.round);
   });
 
   $('#p1-hold').click(function(event){
@@ -57,6 +54,15 @@ $(document).ready(function(){
 
 
 
+// function diceRoll(){
+//   var result = Math.floor((Math.random() * 6) + 1);
+//   if (result === 1) {
+//     return "BYE YOU SUCK!"
+//   } else {
+//   round.push(result);
+//   return result;
+//   }
+// };
 // function score(){
 //     var total = 0;
 //     for (round){
@@ -66,4 +72,15 @@ $(document).ready(function(){
 // function results(){
 //   var math = diceRoll();
 //   return round.push();
+// };
+
+
+//  function endScore(){
+//    var result = score(+ this.total);
+//    round = [];
+//    if (result >= 100) {
+//      return "YAY YOU WIN GET LOST!"
+//    } else {
+//     return result;
+//   }
 // };
